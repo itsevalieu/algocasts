b86077eeba6b7393861d2cdff43a9158a9c0761a
itsevalieu <itsevalieu@gmail.com>
2019-11-09 18:02:48 -0800

    For loop, .reduce(), and .reverse() solutions for reverse string problem

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,28 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let reversed = '';
+    for (char of str) {
+        console.log('char', char);
+        reversed = char + reversed;
+    }
+    return reversed;
+}
 
+reverse('kitty');
 module.exports = reverse;
+
+// return str.split('').reverse().join('');
+
+// let reversed = '';
+// for (char in str) {
+//     console.log('char', str[char]);
+//     reversed = str[char] + reversed;
+// }
+// return reversed;
+
+// return str.split('').reduce((reversed, char) => {
+//     debugger;
+//     return char + reversed;
+// });
