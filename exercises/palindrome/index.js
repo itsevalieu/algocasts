e2e7b6e2867ceda939dd9df85b9c9b30379617cf
itsevalieu <itsevalieu@gmail.com>
2019-11-09 18:01:55 -0800

    For loop solution and .every() solution for palindrome problem

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,18 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    if (str.length === 0 || str.length === 1) return true;
+    return str.split('').every((e, i, arr) => e === arr[arr.length - 1 - i]);
+    // using the every() higher order function to test each element to see if it passes the test given, if one element doesn't it'll pass a false bool value
+}
 
 module.exports = palindrome;
+
+// if (str.length === 0 || str.length === 1) return true;
+// for (let i = 0; i < str.length / 2; i++) {
+//     if (str[i] !== str[str.length - 1 - i]) {
+//         return false;
+//     }
+// }
+// return true;
