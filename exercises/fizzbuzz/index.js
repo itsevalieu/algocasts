2f27b86f25dbb8f81d18e3d48e1f43476ebe9201
itsevalieu <itsevalieu@gmail.com>
2019-11-09 18:41:32 -0800

    For loop, if/else if statement solution for fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,19 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 !== 0 && i % 5 !== 0) {  // not a multiple of three or five, print regular number
+            console.log(i);
+        }
+        else if (i % 3 === 0 && i % 5 === 0) { // if a multiple of three and five, print fizzbuzz
+            console.log('fizzbuzz');
+        } else if (i % 3 === 0) { // if a multiple of three, print fizz
+            console.log('fizz');
+        } else if (i % 5 === 0) { // if a multiple of five, print buzz
+            console.log('buzz');
+        }
+    }
+}
 
 module.exports = fizzBuzz;
